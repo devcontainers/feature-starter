7ddda177e1018aa5e51621a36a030d04f43fae86
Christof Marti <chrmarti@microsoft.com>
2022-08-25 11:25:26 +0200

    Simplify sample scripts

--- src/hello/install.sh
+++ src/hello/install.sh
@@ -1,19 +1,17 @@
-#!/bin/bash
+#!/bin/sh
+set -e
 
 echo "Activating feature 'hello'"
 
 GREETING=${GREETING:-undefined}
 echo "The provided greeting is: $GREETING"
 
-tee /usr/hello.sh > /dev/null \
+cat > /usr/local/bin/hello \
 << EOF
-#!/bin/bash
+#!/bin/sh
 RED='\033[0;91m'
 NC='\033[0m' # No Color
-echo -e -n "\${RED}${GREETING}, \$(whoami)!"
-echo -e -n "\${NC}"
+echo "\${RED}${GREETING}, \$(whoami)!\${NC}"
 EOF
 
-chmod +x /usr/hello.sh
-sudo cat '/usr/hello.sh' > /usr/local/bin/hello
-sudo chmod +x /usr/local/bin/hello
+chmod +x /usr/local/bin/hello
