1769ee90294afc41aafb72a64e33cfd692c492d0
Josh Spicer <josh@joshspicer.com>
2022-11-02 21:17:04 +0000

    checkpoint for expanding with remoteUser

--- test/hello/test.sh
+++ test/hello/test.sh
@@ -1,31 +1,39 @@
-#!/bin/bash
+#!/bin/sh
 
 # This test file will be executed against an auto-generated devcontainer.json that
-# includes the 'hello' feature with no options.
+# includes the 'hello' Feature with no options.
+#
+# For more information, see: https://github.com/devcontainers/cli/blob/main/docs/features/test.md
 #
 # Eg:
 # {
 #    "image": "<..some-base-image...>",
 #    "features": {
 #      "hello": {}
-#    }
+#    },
+#    "remoteUser": "root"
 # }
 #
 # Thus, the value of all options will fall back to the default value in 
-# the feature's 'devcontainer-feature.json'.
+# the Feature's 'devcontainer-feature.json'.
 # For the 'hello' feature, that means the default favorite greeting is 'hey'.
 #
 # These scripts are run as 'root' by default. Although that can be changed
-# with the --remote-user flag.
+# with the '--remote-user' flag.
 # 
-# This test can be run with the following command (from the root of this repo)
+# This test can be run with the following command:
+#
 #    devcontainer features test \ 
-#                   --features hello \
-#                   --base-image mcr.microsoft.com/devcontainers/base:ubuntu .
+#                   --features hello   \
+#                   --remote-user root \
+#                   --skip-scenarios   \
+#                   --base-image mcr.microsoft.com/devcontainers/base:ubuntu \
+#                   /path/to/this/repo
 
 set -e
 
 # Optional: Import test library bundled with the devcontainer CLI
+# Provides the 'check' and 'reportResults' commands.
 source dev-container-features-test-lib
 
 # Feature-specific tests
