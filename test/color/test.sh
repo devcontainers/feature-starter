76bfa55f61110d8ddb04b489a2fb25f287ab653d
Samruddhi Khandale <skhandale@microsoft.com>
2022-12-01 11:34:43 -0800

    fix & update tests

--- test/color/test.sh
+++ test/color/test.sh
@@ -27,7 +27,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "version" color | grep 'my favorite color is red'
+check "version" bash -c "color | grep 'my favorite color is red'"
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
