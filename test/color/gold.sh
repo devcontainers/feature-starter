d9a77df2d7ae3b9a117e9bea6ee24a4421e3ac68
Josh Spicer <josh@joshspicer.com>
2023-01-17 17:29:55 +0000

    best test string

--- test/color/gold.sh
+++ test/color/gold.sh
@@ -10,7 +10,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "version" bash -c "color | grep 'my favorite color is gold'"
+check "execute command" bash -c "color | grep 'my favorite color is gold'"
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
