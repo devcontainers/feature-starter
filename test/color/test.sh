d10fea031f477cd92d43fae686324d672629e38b
jarrodcolburn <jcourtlandcolburn@gmail.com>
2023-03-08 00:20:03 -0600

    Add doc and link

--- test/color/test.sh
+++ test/color/test.sh
@@ -33,12 +33,13 @@
 set -e
 
 # Optional: Import test library bundled with the devcontainer CLI
+# See https://github.com/devcontainers/cli/blob/HEAD/docs/features/test.md#dev-container-features-test-lib
 # Provides the 'check' and 'reportResults' commands.
 source dev-container-features-test-lib
 
 # Feature-specific tests
-# The 'check' command comes from the dev-container-features-test-lib.
-
+# The 'check' command comes from the dev-container-features-test-lib. Syntax is...
+# check <LABEL> <cmd> [args...]
 check "validate favorite color" color | grep 'my favorite color is red'
 
 # Report result
--- test/hello/test.sh
+++ test/hello/test.sh
@@ -33,11 +33,13 @@
 set -e
 
 # Optional: Import test library bundled with the devcontainer CLI
+# See https://github.com/devcontainers/cli/blob/HEAD/docs/features/test.md#dev-container-features-test-lib
 # Provides the 'check' and 'reportResults' commands.
 source dev-container-features-test-lib
 
 # Feature-specific tests
-# The 'check' command comes from the dev-container-features-test-lib.
+# The 'check' command comes from the dev-container-features-test-lib. Syntax is...
+# check <LABEL> <cmd> [args...]
 check "execute command" bash -c "hello | grep 'hey, $(whoami)!'"
 
 # Report results
