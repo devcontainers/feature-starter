61d9ce66625f29b86a53434f4a7d0d8f4f456afb
Aaron Stainback <acehack00@gmail.com>
2023-05-17 05:24:50 +0000

    chmod

--- .devcontainer/src/scripts/setup/set-env-vars.sh
+++ .devcontainer/src/scripts/setup/set-env-vars.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env zsh
 #shellcheck shell=bash
-projectRoot="$(dirname "$(dirname "$(dirname "$(cd -- "$(dirname -- "${BASH_SOURCE-$0}")" &> /dev/null && pwd)")")")"
+projectRoot="$(dirname "$(dirname "$(dirname "$(dirname "$(cd -- "$(dirname -- "${BASH_SOURCE-$0}")" &> /dev/null && pwd)")")")")"
 set -o allexport
 source "$projectRoot/.env"
 set +o allexport
 export PSHELL="pwsh"
 export DEVCONTAINER_FEATURES_PROJECT_ROOT="$projectRoot"
-export DEVCONTAINER_FEATURES_SOURCE_ROOT="$DEVCONTAINER_FEATURES_PROJECT_ROOT/src"
+export DEVCONTAINER_FEATURES_SOURCE_ROOT="$DEVCONTAINER_FEATURES_PROJECT_ROOT/.devcontainer/src"
 export DEVCONTAINER_FEATURES_SCRIPTS_ROOT="$DEVCONTAINER_FEATURES_SOURCE_ROOT/scripts"
