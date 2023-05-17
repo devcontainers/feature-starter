549783c0747f81ace0399552594bebb5c1187f21
Aaron Stainback <acehack00@gmail.com>
2023-05-17 20:10:23 +0000

    Testing

--- test/homebrew/install_forced_brews.sh
+++ test/homebrew/install_forced_brews.sh
@@ -5,6 +5,8 @@ set -e
 # Add Homebrew to PATH
 BREW_PREFIX="${BREW_PREFIX:-"/home/linuxbrew/.linuxbrew"}"
 eval "$("$BREW_PREFIX/bin/brew" shellenv)"
+# Add psql to PATH
+PATH="$PATH:/home/linuxbrew/.linuxbrew/opt/libpq/bin"
 
 # Import test library for `check` command
 source dev-container-features-test-lib
