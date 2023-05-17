90ca351b7a2a0a969ca2342125ed7a0b097470ee
Aaron Stainback <acehack00@gmail.com>
2023-05-17 19:03:18 +0000

    Working on tests

--- test/homebrew/install_brews.sh
+++ test/homebrew/install_brews.sh
@@ -5,11 +5,14 @@ set -e
 # Import test library for `check` command
 source dev-container-features-test-lib
 
+BREW_PREFIX="${BREW_PREFIX:-"/home/linuxbrew/.linuxbrew"}"
+eval "$("$BREW_PREFIX/bin/brew" shellenv)"
+
 # Check to make sure the user is vscode
 check "user is vscode" whoami | grep vscode
 
 # Extension-specific tests
-check "mkcert" bash -c "brew list --formula mkcert"
+check "mkcert" brew list --formula mkcert
 
 # Report result
 reportResults
