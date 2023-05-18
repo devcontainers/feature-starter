4853b47c27e42af6cfe84d0453b85da1807b4168
Aaron Stainback <acehack00@gmail.com>
2023-05-18 18:56:21 -0400

    Fixing test

--- .devcontainer/features/test/homebrew/install_forced_brews.sh
+++ .devcontainer/features/test/homebrew/install_forced_brews.sh
@@ -2,12 +2,6 @@
 #shellcheck source=/dev/null
 set -e
 
-mustroot='Script must be run as root user.'
-if [ "$(id -u)" -ne 0 ]; then
-    echo -e "$mustroot"
-    exit 1
-fi
-
 # Import test library for `check` command
 source dev-container-features-test-lib
 
