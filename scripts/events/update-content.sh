dd851349bb6f1e180a1b8c3dce3461deea9abb32
Aaron Stainback <aaron@alephz.ai>
2023-05-23 05:27:09 -0400

    Working on devspace

--- scripts/events/update-content.sh
+++ scripts/events/update-content.sh
@@ -3,5 +3,5 @@
 set -e
 rm -f nohup.out
 rm -f gcm-diagnose.log
-"$DEVCONTAINER_FEATURES_PROJECT_ROOT/run" -s setup devspace 
+"$DEVCONTAINER_FEATURES_PROJECT_ROOT/run" setup devspace 
 echo "Press Ctrl+Shift+~ to open a terminal in the current dev container"
