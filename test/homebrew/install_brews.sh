9c792d848309716d4650e24b113cbc1e91fb5cfb
Aaron Stainback <aaron@alephz.ai>
2023-05-23 20:59:12 -0400

    Simplifying test

--- test/homebrew/install_brews.sh
+++ test/homebrew/install_brews.sh
@@ -3,10 +3,9 @@
 #example=https://github.com/devcontainers/features/blob/main/test/azure-cli/test.sh
 #example=https://github.com/meaningful-ooo/devcontainer-features/blob/main/test/homebrew/test.sh
 set -e
-BREW_PREFIX="${BREW_PREFIX:-"/home/linuxbrew/.linuxbrew"}"
-
+# refresh environment profile
+reset
 source /etc/os-release
-eval "$("$BREW_PREFIX/bin/brew" shellenv)"
 
 # Import test library for `check` command
 echo "Installing test library..."
