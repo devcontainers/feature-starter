7a7527266e0f4d784f474dab9240e05c3bcaa512
Aaron Stainback <aaron@alephz.ai>
2023-05-25 17:32:43 -0400

    working on tests

--- test/bash-command/sudo_and_user.sh
+++ test/bash-command/sudo_and_user.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+#shellcheck disable=SC2016
 #shellcheck source=/dev/null
 
 set -e
@@ -7,7 +8,7 @@ source dev-container-features-test-lib
 
 
 check "echo \$HELLO" [ "$(source /etc/environment && echo "$HELLO")" == "5" ]
-check "echo \$CURRENT_USER" [ "$(sudo -u vscode -i -- 'source $HOME/.bashrc && echo $CURRENT_USER')" == "vscode" ]
+check "echo \$CURRENT_USER" [ "$(su vscode -c 'source $HOME/.bashrc && echo $CURRENT_USER')" == "vscode" ]
 
 
 reportResults
