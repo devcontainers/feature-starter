d3494998626c83775898f27221e8c35c11bd036d
Aaron Stainback <aaron@alephz.ai>
2023-05-25 16:04:35 -0400

    working on tests

--- test/bash-command/set_rc_user.sh
+++ test/bash-command/set_rc_user.sh
@@ -6,7 +6,7 @@ set -e
 source dev-container-features-test-lib
 
 
-su "vscode" -c "check \"echo $CURRENT_USER\" [ \"bash\", \"-l\", \"-c\", \"$(source ~/.bashrc && echo \"\$CURRENT_USER\")\" == \"$(whoami)\" ]"
+check "echo \$CURRENT_USER" "su \"vscode\" -c "bash -l -c "$(source ~/.bashrc && echo "$CURRENT_USER")" == "$(whoami)"""
 
 
 reportResults
--- test/bash-command/sudo_and_user.sh
+++ test/bash-command/sudo_and_user.sh
@@ -6,8 +6,8 @@ set -e
 source dev-container-features-test-lib
 
 
-check "echo $HELLO" [ "$(source /etc/environment && echo "$HELLO")" == "5" ]
-su "vscode" -c "check \"echo $CURRENT_USER\" [ \"bash\", \"-l\", \"-c\", \"$(source ~/.bashrc && echo \""$CURRENT_USER"\")\" == \"$(whoami)\" ]"
+check "echo \$HELLO" [ "$(source /etc/environment && echo "$HELLO")" == "5" ]
+check "echo \$CURRENT_USER" "su \"vscode\" -c "bash -l -c "$(source ~/.bashrc && echo "$CURRENT_USER")" == "$(whoami)"""
 
 
 reportResults
