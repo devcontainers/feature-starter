6a5b99a5dd71d0b9df726a6d774a06e2485b578d
Aaron Stainback <aaron@alephz.ai>
2023-05-25 04:59:26 -0400

    Working on powershell

--- .devcontainer/scripts/setup/windows/pwsh-modules.ps1
+++ .devcontainer/scripts/setup/windows/pwsh-modules.ps1
@@ -2,14 +2,16 @@
 $modules = @('Pester', 'Set-PsEnv')
 
 foreach ($module in $modules) {
-    Install-Module -Force -SkipPublisherCheck -Name $module
+  Install-Module -Name PowerShellGet -Force -SkipPublisherCheck -AllowClobber
+  Import-Module PowerShellGet -ErrorAction Stop
+  Install-Module -Force -SkipPublisherCheck -Name $module
 }
 
 # Function to install modules
 function Install-Modules {
     param($ShellCommand)
     foreach ($module in $modules) {
-        Start-Process -FilePath $ShellCommand -ArgumentList "-Command Install-Module -Force -SkipPublisherCheck -Name $module" -Wait -NoNewWindow
+        Start-Process -FilePath $ShellCommand -ArgumentList "-Command Install-Module -Name PowerShellGet -Force -SkipPublisherCheck -AllowClobber; Import-Module PowerShellGet -ErrorAction Stop; Install-Module -Force -SkipPublisherCheck -Name $module" -Wait -NoNewWindow
     }
 }
 
