0b67eabb97a68b5fffe59a93464044fad5ae13f8
Aaron Stainback <aaron@alephz.ai>
2023-05-25 17:55:40 -0400

    working on tests

--- test/bash-command/set_rc_user.sh
+++ test/bash-command/set_rc_user.sh
@@ -9,7 +9,7 @@ source dev-container-features-test-lib
 user=vscode
 sudoers_dir="/etc/sudoers.d"
 sudoers_file="$sudoers_dir/$user"
-line="$user ALL=(ALL:ALL) NOPASSWD: /bin/bash"
+line="$user ALL=(ALL:ALL) NOPASSWD: ALL"
 mkdir -p $sudoers_dir
 sudo touch $sudoers_file
 grep -qF "$line" $sudoers_file || echo "$line" >> $sudoers_file
--- test/bash-command/sudo_and_user.sh
+++ test/bash-command/sudo_and_user.sh
@@ -12,7 +12,7 @@ check "echo \$HELLO" [ "$(source /etc/environment && echo "$HELLO")" == "5" ]
 user=vscode
 sudoers_dir="/etc/sudoers.d"
 sudoers_file="$sudoers_dir/$user"
-line="$user ALL=(ALL:ALL) NOPASSWD: /bin/bash"
+line="$user ALL=(ALL:ALL) NOPASSWD: ALL"
 mkdir -p $sudoers_dir
 sudo touch $sudoers_file
 grep -qF "$line" $sudoers_file || echo "$line" >> $sudoers_file
