7cfe3845697ba768194783ba9cc7ba60b077a0d0
Aaron Stainback <aaron@alephz.ai>
2023-05-25 18:01:07 -0400

    working on tests

--- test/bash-command/sudo_and_user.sh
+++ test/bash-command/sudo_and_user.sh
@@ -9,15 +9,16 @@ source dev-container-features-test-lib
 
 check "echo \$HELLO" [ "$(source /etc/environment && echo "$HELLO")" == "5" ]
 
+echo -e "$(whoami)"
 user=vscode
 sudoers_dir="/etc/sudoers.d"
 sudoers_file="$sudoers_dir/$user"
 line="$user ALL=(ALL:ALL) NOPASSWD: ALL"
-mkdir -p $sudoers_dir
+sudo mkdir -p $sudoers_dir
 sudo touch $sudoers_file
-grep -qF "$line" $sudoers_file || echo "$line" >> $sudoers_file
-chmod 0440 $sudoers_file
-check "echo \$CURRENT_USER $(whoami)" [ "$(su "$user" -c 'source $HOME/.bashrc && echo $CURRENT_USER')" == "$user" ]
+sudo grep -qF "$line" $sudoers_file || echo "$line" | sudo tee --append $sudoers_file
+sudo chmod 0440 $sudoers_file
+check "echo \$CURRENT_USER" [ "$(su "$user" -c 'source $HOME/.bashrc && echo $CURRENT_USER')" == "$user" ]
 
 
 reportResults
