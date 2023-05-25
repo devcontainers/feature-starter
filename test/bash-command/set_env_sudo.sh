a1cc873766cad935a7f85e1b81c0ee4034c66649
Aaron Stainback <aaron@alephz.ai>
2023-05-25 15:37:53 -0400

    Working tests

---
+++ test/bash-command/set_env_sudo.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#shellcheck source=/dev/null
+
+set -e
+
+source dev-container-features-test-lib
+
+
+check "echo $HELLO" [ "$(source /etc/environment && echo "$HELLO")" == "5" ]
+su "vscode" -c check "echo $CURRENT_USER" [ "bash", "-l", "-c", "$(source ~/.bashrc && echo "$CURRENT_USER")" == "$(whoamsi))" ]
+
+
+reportResults
---
+++ test/bash-command/sudo_and_user.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#shellcheck source=/dev/null
+
+set -e
+
+source dev-container-features-test-lib
+
+
+su "vscode" -c check "echo $CURRENT_USER" [ "bash", "-l", "-c", "$(source ~/.bashrc && echo "$CURRENT_USER")" == "$(whoamsi))" ]
+
+
+reportResults
