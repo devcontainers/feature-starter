38b8894292e49fa1f6dc8dded95ae65726c643df
Aaron Stainback <aaron@alephz.ai>
2023-05-25 17:52:50 -0400

    working on tests

--- test/bash-command/set_rc_user.sh
+++ test/bash-command/set_rc_user.sh
@@ -11,7 +11,7 @@ sudoers_dir="/etc/sudoers.d"
 sudoers_file="$sudoers_dir/$user"
 line="$user ALL=(ALL:ALL) NOPASSWD: /bin/bash"
 mkdir -p $sudoers_dir
-touch $sudoers_file
+sudo touch $sudoers_file
 grep -qF "$line" $sudoers_file || echo "$line" >> $sudoers_file
 chmod 0440 $sudoers_file
 check "echo \$CURRENT_USER" [ "$(su "$user" -c 'source $HOME/.bashrc && echo $CURRENT_USER')" == "$user" ]
