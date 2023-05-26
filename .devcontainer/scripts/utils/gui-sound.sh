c0a6f2655a67b9f937fe8c71b194751779de0ead
Aaron Stainback <aaron@alephz.ai>
2023-05-26 06:06:22 -0400

    Working on init

--- .devcontainer/scripts/utils/gui-sound.sh
+++ .devcontainer/scripts/utils/gui-sound.sh
@@ -1,25 +1,31 @@
 #!/usr/bin/env bash
 export DISPLAY=${DISPLAY:=host.docker.internal:0}
-export WAYLAND_DISPLAY=${WAYLAND_DISPLAY:=wayland-0}
-mkdir -p /tmp/.X11-unix
+export WAYLAND_DISPLAY=${WAYLAND_DISPLAY:=}
 export X11=${X11:=/tmp/.X11-unix}
-mkdir -p /tmp/runtime-$USER
-export XDG_RUNTIME_DIR=${XDG_RUNTIME_DIR:=/tmp/runtime-$USER}
-mkdir -p /tmp/pulse/native
+export XDG_RUNTIME_DIR=${XDG_RUNTIME_DIR:="/tmp/runtime-$USER"}
 export PULSE_SERVER=${PULSE_SERVER:=/tmp/pulse/native}
-if [ -e "/mnt/wslg" ]; then
-    WSLG=${WSLG:=/mnt/wslg}
-else
-    mkdir -p /tmp/wslg
-    WSLG=${WSLG:=/tmp/wslg}
+export WSLG=${WSLG:/tmp/wslg}
+export LIB_WSL=${LIB_WSL:=/tmp/lib/wsl}
+if ! [ -e /tmp/.X11-unix ]; then
+  mkdir -p /tmp/.X11-unix
 fi
 
-export WSLG="$WSLG"
-if [ -e "/usr/lib/wsl" ]; then
-    LIB_WSL=${LIB_WSL:=/usr/lib/wsl}
-else
-    mkdir -p /tmp/lib/wsl
-    LIB_WSL=${LIB_WSL:=/tmp/lib/wsl}
+if ! [ -e "$XDG_RUNTIME_DIR" ]; then
+  mkdir -p "$XDG_RUNTIME_DIR"
 fi
 
-export LIB_WSL="$LIB_WSL"
+if ! [ -e "$PULSE_SERVER" ]; then
+  mkdir -p "$PULSE_SERVER"
+fi
+
+if [ -e /mnt/wslg ]; then
+    export WSLG=/mnt/wslg
+else
+    mkdir -p "$WSLG"
+fi
+
+if [ -e /usr/lib/wsl ]; then
+    export LIB_WSL=/usr/lib/wsl
+else
+    mkdir -p "$LIB_WSL"
+fi
