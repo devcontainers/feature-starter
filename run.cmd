f754a45bfd4faa94be041323d422bd49f4d3da9d
Aaron Stainback <aaron@alephz.ai>
2023-05-26 16:19:56 -0400

    Working on init

--- run.cmd
+++ run.cmd
@@ -1,17 +1,10 @@
 @ECHO OFF
-setlocal
 set projectRoot=%~dp0
 set scriptPath=%1
 set script=%2
 set commandPath=%3
 set command=%4
 set "scriptsRoot=%projectRoot%/.devcontainer/scripts"
-call "%scriptsRoot%/setup/environment.cmd"
+FOR /F "tokens=1* delims==" %%A IN ('call "%scriptsRoot%/setup/environment.cmd"') DO SET "%%A=%%B"
 rem "%DEVCONTAINER_SCRIPTS_ROOT%/utils/gui-sound.cmd"
-call :Run "%DEVCONTAINER_FEATURES_PROJECT_ROOT%/run.ps1" "%scriptPath%" "%script%" "%commandPath%" "%command%"
-goto :eof
-:Run
-echo %1
-PowerShell -file "%1" --scriptPath "%2" --script "%3" --commandPath "%4" --command "%5"
-endlocal
-exit /b
+PowerShell -file "%DEVCONTAINER_FEATURES_PROJECT_ROOT%/run.ps1" --scriptPath "%scriptPath%" --script "%script%" --commandPath "%commandPath%" --command "%command%"
