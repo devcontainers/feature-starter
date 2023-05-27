75d37ae5c372be1e1b8926176c775e2063f46b3a
Aaron Stainback <aaron@alephz.ai>
2023-05-27 18:29:10 -0400

    Working on linux clean

--- .devcontainer/scripts/devspace/exec.sh
+++ .devcontainer/scripts/devspace/exec.sh
@@ -3,6 +3,6 @@
 set -e
 commandPath="$1"
 command="$2"
-"$DEVCONTAINER_FEATURES_PROJECT_ROOT/run" -s devspace up
+"$DEVCONTAINER_FEATURES_PROJECT_ROOT/run" devspace up
 containerid=$(docker ps -q -f name="$DEVCONTAINER_PROJECT_NAME-devspace")
 devcontainer exec --container-id "$containerid" zsh -l -c "\$DEVCONTAINER_FEATURES_PROJECT_ROOT/run $commandPath $command"
--- .devcontainer/scripts/devspace/system-clean.sh
+++ .devcontainer/scripts/devspace/system-clean.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #shellcheck shell=bash
-"$DEVCONTAINER_FEATURES_PROJECT_ROOT/run" -s devspace clean
+"$DEVCONTAINER_FEATURES_PROJECT_ROOT/run" devspace clean
 docker system prune -a -f --volumes
