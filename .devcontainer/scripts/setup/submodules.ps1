f5c155fa15d1b16f334f280a29fed4e249abc9c7
Aaron Stainback <aaron@alephz.ai>
2023-05-28 18:57:39 -0400

    Working on setup

--- .devcontainer/scripts/setup/submodules.ps1
+++ .devcontainer/scripts/setup/submodules.ps1
@@ -1,13 +1,22 @@
 # Update submodules
+$initSubmodules = $false
+$submodulePath = "$env:DEVCONTAINER_FEATURES_PROJECT_ROOT\.devcontainer\dependencies\devcontainers\features"
+if (Test-Path $submodulePath) {
+  $directoryInfo = Get-ChildItem $submodulePath | Measure-Object
+  if ($directoryInfo.count -eq 0) {
+    $initSubmodules = $true
+  }
+}
+
 Push-Location "$env:DEVCONTAINER_FEATURES_PROJECT_ROOT"
 try {
+  if ($initSubmodules) {
+    git submodule sync --recursive
+    git submodule update --init --recursive
+    git submodule foreach --recursive git checkout main
+  }
+  
   git submodule foreach --recursive git pull
-  if ($LASTEXITCODE -ne 0) { throw "Exit code is $LASTEXITCODE" }
-} catch {
-  git submodule sync --recursive
-  git submodule update --init --recursive
-  git submodule foreach --recursive git checkout main
 } finally {
-  git submodule foreach --recursive git pull
   Pop-Location
 }
--- .devcontainer/scripts/setup/submodules.sh
+++ .devcontainer/scripts/setup/submodules.sh
@@ -1,8 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Update submodules
+init_submodules="false"
+submodule_path="$DEVCONTAINER_FEATURES_PROJECT_ROOT/.devcontainer/dependencies/devcontainers/features"
+if [ -d "$submodule_path" ]; then
+  directory_info=$(ls -A "$submodule_path")
+  if [ -z "$directory_info" ]; then
+    init_submodules="true"
+  fi
+fi
+
 pushd "$DEVCONTAINER_FEATURES_PROJECT_ROOT" || exit
-if ! git submodule foreach --recursive git pull; then
+if [ "$init_submodules" = "true" ]; then
   git submodule sync --recursive
   git submodule update --init --recursive
   git submodule foreach --recursive git checkout main
