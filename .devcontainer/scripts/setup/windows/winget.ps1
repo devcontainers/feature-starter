aff08081838cd5f60305c1cb37555e8539194533
Aaron Stainback <aaron@alephz.ai>
2023-05-28 20:34:29 -0400

    Working on windows setup

--- .devcontainer/scripts/setup/windows/winget.ps1
+++ .devcontainer/scripts/setup/windows/winget.ps1
@@ -20,41 +20,8 @@ wsl --set-default-version 2
 winget install --name --exact "Ubuntu" --accept-package-agreements --accept-source-agreements --disable-interactivity
 winget install --name "Ubuntu (Preview)" --accept-package-agreements --accept-source-agreements --disable-interactivity
 winget install --id "Docker.DockerDesktop" --accept-package-agreements --accept-source-agreements --disable-interactivity
-$ProgPref = $ProgressPreference
-$ProgressPreference = 'SilentlyContinue'
-$results = Enable-WindowsOptionalFeature -FeatureName TFTP,LegacyComponents,DirectPlay,MediaPlayback,SmbDirect,MSRDC-Infrastructure,MicrosoftWindowsPowerShellV2Root,MicrosoftWindowsPowerShellV2,SearchEngine-Client-Package,Printing-PrintToPDFServices-Features,Printing-XPSServices-Features,TelnetClient,Printing-Foundation-InternetPrinting-Client,VirtualMachinePlatform,Containers-DisposableClientVM -All -Online -NoRestart -WarningAction SilentlyContinue
-$ProgressPreference = $ProgPref
-if ($results.RestartNeeded -eq $true) {
-  Restart-Computer -Force
-}
-
-Add-WindowsCapability -Online -Name "App.StepsRecorder"
-Add-WindowsCapability -Online -Name "App.Support.QuickAssist"
-Add-WindowsCapability -Online -Name "App.WirelessDisplay.Connect"
-Add-WindowsCapability -Online -Name "DirectX.Configuration.Database"
-Add-WindowsCapability -Online -Name "Language.Basic~~~en-US"
-Add-WindowsCapability -Online -Name "Language.Handwriting~~~en-US"
-Add-WindowsCapability -Online -Name "Language.OCR~~~en-US"
-Add-WindowsCapability -Online -Name "Language.Speech~~~en-US"
-Add-WindowsCapability -Online -Name "Language.TextToSpeech~~~en-US"
-Add-WindowsCapability -Online -Name "MathRecognizer"
-Add-WindowsCapability -Online -Name "Microsoft.WebDriver"
-Add-WindowsCapability -Online -Name "Microsoft.Windows.MSPaint"
-Add-WindowsCapability -Online -Name "Microsoft.Windows.Notepad"
-Add-WindowsCapability -Online -Name "Microsoft.Windows.PowerShell.ISE"
-Add-WindowsCapability -Online -Name "Microsoft.Windows.WordPad"
-Add-WindowsCapability -Online -Name "Msix.PackagingTool.Driver"
-Add-WindowsCapability -Online -Name "OpenSSH.Client"
-Add-WindowsCapability -Online -Name "OpenSSH.Server"
-Add-WindowsCapability -Online -Name "Print.Fax.Scan"
-Add-WindowsCapability -Online -Name "Print.Management.Console"
-Add-WindowsCapability -Online -Name "RasCMAK.Client"
-Add-WindowsCapability -Online -Name "RIP.Listener"
-Add-WindowsCapability -Online -Name "SNMP.Client"
-Add-WindowsCapability -Online -Name "Tools.DeveloperMode.Core"
-Add-WindowsCapability -Online -Name "Tools.Graphics.DirectX"
-Add-WindowsCapability -Online -Name "WMI-SNMP-Provider.Client"
-Add-WindowsCapability -Online -Name "XPS.Viewer"
+# Windows Features
+& "$env:DEVCONTAINER_FEATURES_PROJECT_ROOT/run.ps1" setup/windows pre-sudo
 winget install --name "Microsoft Visual Studio Code" --accept-package-agreements --accept-source-agreements --disable-interactivity
 winget install --name "Microsoft Visual Studio Code Insiders" --accept-package-agreements --accept-source-agreements --disable-interactivity
 # https://learn.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio?view=vs-2022
