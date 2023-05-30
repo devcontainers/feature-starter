54ddaf62815c676c9c1127e4229c9b2572ef9d0a
Aaron Stainback <aaron@alephz.ai>
2023-05-30 00:50:25 -0400

    Working on dockerfile

---
+++ .devcontainer/scripts/docker/create-user.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash zsh
+
+USERNAME="$1"
+USER_UID="$2"
+USER_GID="$3"
+
+if id "$USERNAME"; then 
+    echo "User $USERNAME already exists"
+else
+    echo "Creating user $USERNAME"
+    groupadd --gid $USER_GID $USERNAME
+    useradd --uid $USER_UID --gid $USER_GID -m $USERNAME
+    apt-get update
+    apt-get install -y sudo
+    echo $USERNAME ALL=\(root\) NOPASSWD:ALL > /etc/sudoers.d/$USERNAME
+    chmod 0440 /etc/sudoers.d/$USERNAME
+fi
