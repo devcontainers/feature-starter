0a4d1d067d560ec151ea9d70448c81200a060ebc
Aaron Stainback <aaron@alephz.ai>
2023-05-30 06:21:55 -0400

    WOT

--- test/user-command/test.sh
+++ test/user-command/test.sh
@@ -6,7 +6,7 @@ set -e
 source dev-container-features-test-lib
 
 
-check "echo \$TEST" [ "$((source /etc/environment || source "$HOME/.bashrc") && echo "$TEST")" == "test" ]
+check "echo \$TEST" [ "$(source /etc/environment || source "$HOME/.bashrc" && echo "$TEST")" == "test" ]
 
 
 reportResults
