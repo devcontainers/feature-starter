813e8e39edbf05941b673659a95439a9a728f35e
Aaron Stainback <aaron@alephz.ai>
2023-05-30 06:07:42 -0400

    Working on user-command

--- src/user-command/install.sh
+++ src/user-command/install.sh
@@ -2,24 +2,7 @@
 #shellcheck disable=SC2086
 set -ex
 
-USERNAME="${USERNAME:-"automatic"}"
-
-# Determine the appropriate non-root user.
-if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
-  USERNAME=""
-  POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
-  for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
-    if id -u "${CURRENT_USER}" >/dev/null 2>&1; then
-      USERNAME="${CURRENT_USER}"
-      break
-    fi
-  done
-  if [ "${USERNAME}" = "" ]; then
-    USERNAME=root
-  fi
-elif [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} >/dev/null 2>&1; then
-  USERNAME=root
-fi
+USERNAME="$(whoami)"
 
 # Setup command
 if [ "$USERNAME" = "root" ]; then
