c623f3c306661ca54264bcedf3343cf05bc10d10
Aaron Stainback <aaron@alephz.ai>
2023-05-30 06:20:52 -0400

    WOT

--- test/user-command/test.sh
+++ test/user-command/test.sh
@@ -6,11 +6,7 @@ set -e
 source dev-container-features-test-lib
 
 
-if [ "$USERNAME" = "root" ]; then
-    check "echo \$TEST" [ "$(source /etc/environment && echo "$TEST")" == "test" ]
-else
-    check "echo \$TEST" [ "$(source "$HOME/.bashrc" && echo "$TEST")" == "test" ]
-fi
+check "echo \$TEST" [ "$((source /etc/environment || source "$HOME/.bashrc") && echo "$TEST")" == "test" ]
 
 
 reportResults
