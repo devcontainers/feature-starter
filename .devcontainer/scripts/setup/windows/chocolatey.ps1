496b7e6f22ea12bcd68a2f14167f470ef23d9975
Aaron Stainback <aaron@alephz.ai>
2023-05-31 00:40:04 -0400

    Adding profile creation

--- .devcontainer/scripts/setup/windows/chocolatey.ps1
+++ .devcontainer/scripts/setup/windows/chocolatey.ps1
@@ -6,6 +6,7 @@ try {
 } catch {
   Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
   refreshenv
+  . $profile
 }
 
 choco feature enable -n allowGlobalConfirmation
