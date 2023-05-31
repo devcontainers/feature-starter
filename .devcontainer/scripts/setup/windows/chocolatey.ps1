8eb68f9ef4c17ee34326e0286f672f2818ac7338
Aaron Stainback <aaron@alephz.ai>
2023-05-30 23:47:16 -0400

    Adding refresh env

--- .devcontainer/scripts/setup/windows/chocolatey.ps1
+++ .devcontainer/scripts/setup/windows/chocolatey.ps1
@@ -4,6 +4,7 @@ try {
   if ($LASTEXITCODE -ne 0) { throw "Exit code is $LASTEXITCODE" }
 } catch {
   Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+  refreshenv
 }
 
 $env:ChocolateyInstall = [Environment]::GetEnvironmentVariable("ChocolateyInstall", [EnvironmentVariableTarget]::Machine)
