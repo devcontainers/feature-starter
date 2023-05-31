2bed4bd78e956ffb9e0abc6fd037be329ad835af
Aaron Stainback <aaron@alephz.ai>
2023-05-31 05:32:34 -0400

    Adding retries

--- .devcontainer/scripts/setup/devspace/pre-build-sudo.sh
+++ .devcontainer/scripts/setup/devspace/pre-build-sudo.sh
@@ -10,13 +10,15 @@
   grep -qxF "$line" "$file" || echo "$line" >> "$file"
   chmod 440 "$file"
 # Update apt-packages
-  apt update
-  apt upgrade -y
-  apt install -y --install-recommends --fix-broken --fix-missing
-  apt install -y --install-recommends sudo systemd mawk gawk bash zsh file sed curl wget grep bzip2 build-essential make cmake gcc g++ less locales
-  apt install -y --install-recommends patch tzdata uuid-runtime netbase python3 dotnet-sdk-6.0 dotnet-sdk-7.0 git apt-transport-https ca-certificates age
-  apt install -y --install-recommends openssl openssh-client procps checkinstall dos2unix software-properties-common libnss3 libnss3-tools shellcheck jq moreutils
-  apt install -y --install-recommends bash-completion zlib1g-dev speedtest-cli powerline fonts-powerline fonts-dejavu-core gedit gimp nautilus vlc x11-apps
+  while ! (
+    apt update
+    apt upgrade -y
+    apt install -y --install-recommends --fix-broken --fix-missing
+    apt install -y --install-recommends sudo systemd mawk gawk bash zsh file sed curl wget grep bzip2 build-essential make cmake gcc g++ less locales
+    apt install -y --install-recommends patch tzdata uuid-runtime netbase python3 dotnet-sdk-6.0 dotnet-sdk-7.0 git apt-transport-https ca-certificates age
+    apt install -y --install-recommends openssl openssh-client procps checkinstall dos2unix software-properties-common libnss3 libnss3-tools shellcheck jq moreutils
+    apt install -y --install-recommends bash-completion zlib1g-dev speedtest-cli powerline fonts-powerline fonts-dejavu-core gedit gimp nautilus vlc x11-apps
+  ) do echo "Retrying apt-packages..."; done
 # Install docker completions
   rm -rf /etc/bash_completion.d/docker.sh || true
   mkdir -p /etc/bash_completion.d
