afc721360cf68e4ad63179f46636cfc1edfca977
mary <58061556+mar-liu@users.noreply.github.com>
2024-08-21 12:45:21 +0000

    Test release

--- test/_global/color_and_hello.sh
+++ test/_global/color_and_hello.sh
@@ -4,7 +4,7 @@
 #
 # This test file is executed against a running container constructed
 # from the value of 'color_and_hello' in the tests/_global/scenarios.json file.
-#
+# 
 # The value of a scenarios element is any properties available in the 'devcontainer.json'.
 # Scenarios are useful for testing specific options in a feature, or to test a combination of features.
 # 
